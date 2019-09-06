fba968d4d7e26316b2af67d11ab36aff26ff4877
Cristiano Ghersi <cghersi@liquidtext.net>
2019-09-06 12:29:54 -0700

    added SizeOfTextBox

--- ClippedCanvas/ClippedCanvas/MainPage.xaml.cs
+++ ClippedCanvas/ClippedCanvas/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Numerics;
+using Windows.Foundation;
 using Windows.UI.Composition;
 using Windows.UI.Core;
 using Windows.UI.Input.Inking;
@@ -8,6 +9,7 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Hosting;
 using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
 using Microsoft.Graphics.Canvas.Geometry;
 // ReSharper disable InconsistentNaming
 
@@ -23,8 +25,11 @@ namespace ClippedCanvas
 		private readonly InkCanvas m_inkCanvas = new InkCanvas();
 		private int m_strokeCount = 0;
 		private int m_clickCount = 0;
-
 		private float m_radius = 0;
+
+
+		private readonly InkCanvas m_mainInkCanvas = new InkCanvas();
+
 
 		public MainPage()
 		{
@@ -33,9 +38,18 @@ namespace ClippedCanvas
 			m_inkCanvas.Width = Workspace.Width;
 			m_inkCanvas.Height = Workspace.Height;
 			Workspace.Children.Add(m_inkCanvas);
-			//Workspace.Children.Insert(0, m_inkCanvas);
 			m_inkCanvas.InkPresenter.InputDeviceTypes = CoreInputDeviceTypes.Pen | CoreInputDeviceTypes.Mouse;
 			m_inkCanvas.InkPresenter.StrokesCollected += InkPresenter_StrokesCollected;
+
+			m_mainInkCanvas.IsHitTestVisible = false;
+			m_mainInkCanvas.Width = LTInkMainManager.Width;
+			m_mainInkCanvas.Height = LTInkMainManager.Height;
+			LTInkMainManager.Children.Add(m_mainInkCanvas);
+			//m_mainInkCanvas.Width = Desktop.Width;
+			//m_mainInkCanvas.Height = Desktop.Height;
+			//Desktop.Children.Add(m_mainInkCanvas);
+			m_mainInkCanvas.InkPresenter.InputDeviceTypes = CoreInputDeviceTypes.Pen | CoreInputDeviceTypes.Mouse;
+			m_mainInkCanvas.InkPresenter.StrokesCollected += InkPresenter_StrokesCollected;
 
 			Explanation.Text =
 				"1. The InkCanvas is the area in AntiqueWhite. But it has a circular Clip, therefore you can draw ink strokes only in a circle near the button 'Click'." +
@@ -89,8 +103,9 @@ namespace ClippedCanvas
 
 			foreach (FrameworkElement excl in excluded)
 			{
-				CanvasGeometry excludedGeom = CanvasGeometry.CreateRectangle(null, 
-					(float)excl.Margin.Left, (float)excl.Margin.Top, (float)excl.ActualWidth, (float)excl.ActualHeight);
+				Rect rect = new Rect(0, 0, (float)excl.ActualWidth, (float)excl.ActualHeight);
+				Rect forGeom = excl.ConvertSafeSlow(rect, target);
+				CanvasGeometry excludedGeom = CanvasGeometry.CreateRectangle(null, forGeom);
 				clipGeometry = clipGeometry.CombineWith(excludedGeom, Matrix3x2.Identity, CanvasGeometryCombine.Exclude);
 			}
 			visual.Clip = comp.CreateGeometricClip(clipGeometry);
@@ -104,6 +119,10 @@ namespace ClippedCanvas
 		private void ExpandBtnWithHoles_OnClick(object _1, RoutedEventArgs _2)
 		{
 			ClipAreaWithHoles(m_inkCanvas, ClickBtn, ClickBtn2, PanelBtn);
+			ClipAreaWithHoles(m_mainInkCanvas, TitleBarCloseBtn);
+			//ClipAreaWithHoles(m_mainInkCanvas, TitleBarPanel);
+			//ClipAreaWithHoles(m_mainInkCanvas, LTMultiDocContainerView);
+			//ClipAreaWithHoles(m_mainInkCanvas, MultiDocView);
 		}
 
 		private void PanelBtn_OnClick(object _1, PointerRoutedEventArgs _2)
@@ -158,6 +177,30 @@ namespace ClippedCanvas
 			double time3 = sw.Elapsed.TotalMilliseconds;
 			TimeResults.Text = string.Format("First: {0}ms; Exclusions: {1}ms; geomClip: {2}ms", time1, time2, time3);
 		}
+
+		private void TitleBarCloseBtn_OnClick(object _1, RoutedEventArgs _2)
+		{
+			m_clickCount++;
+			ShowClickCount();
+		}
+
+		private void TitleBarPanel_OnPointerPressed(object _1, RoutedEventArgs _2)
+		{
+			m_clickCount++;
+			ShowClickCount();
+		}
+
+		private void LTMultiDocContainerView_OnPointerPressed(object _1, RoutedEventArgs _2)
+		{
+			m_clickCount++;
+			ShowClickCount();
+		}
+
+		private void MultiDocView_OnPointerPressed(object _1, RoutedEventArgs _2)
+		{
+			m_clickCount++;
+			ShowClickCount();
+		}
 	}
 
 	public static class Utils
@@ -172,6 +215,15 @@ namespace ClippedCanvas
 			CompositionPathGeometry pathGeometry = compositor.CreatePathGeometry(path);
 			// Create the CompositionGeometricClip
 			return compositor.CreateGeometricClip(pathGeometry);
+		}
+
+		public static Rect ConvertSafeSlow(this UIElement fromView, Rect input, UIElement toView)
+		{
+			if ((toView == null) || (fromView == null))
+				return input;
+			GeneralTransform transform = fromView.TransformToVisual(toView);
+			Rect res = transform.TransformBounds(input);
+			return res;
 		}
 	}
 }
---
+++ SizeOfTextBox/SizeOfTextBox/MainPage.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SizeOfTextBox
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainPage : Page
+	{
+		public static readonly FontFamily FONT_FAMILY = new FontFamily("Arial"); //"new FontFamily("Assets/Fonts/paltn.ttf#Palatino-Roman");
+		public const int FONT_SIZE = 10;
+		public const int LINE_HEAD_INDENT = 3;
+		public const float LINE_SPACING = 1.08f;
+		private static readonly Color FONT_FOREGROUND_COLOR = Color.FromArgb(255, 59, 52, 26);
+		public static readonly SolidColorBrush FONT_FOREGROUND = new SolidColorBrush(FONT_FOREGROUND_COLOR);
+		public const int MIN_WIDTH = 100;
+		public const int MIN_HEIGHT = 50;
+
+		private int offsetY = 100;
+
+		private RichEditBox b;
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+		{
+			b = new RichEditBox
+			{
+				FontFamily = FONT_FAMILY,
+				FontSize = FONT_SIZE,
+				Padding = new Thickness(0),
+				TextWrapping = TextWrapping.Wrap,
+				Foreground = FONT_FOREGROUND,
+				BorderThickness = new Thickness(0),
+				AcceptsReturn = true,
+				AllowFocusWhenDisabled = false
+			};
+			ScrollViewer.SetVerticalScrollBarVisibility(b, ScrollBarVisibility.Hidden);
+			ScrollViewer.SetHorizontalScrollBarVisibility(b, ScrollBarVisibility.Hidden);
+
+			b.Width = MIN_WIDTH;
+			b.Height = MIN_HEIGHT;
+			b.Margin = new Thickness(100, offsetY, 0, 0);
+			offsetY += 100;
+			MainContainer.Children.Add(b);
+
+			InitHeight.Text = "Init H:" + GetElemHeight(b);
+		}
+
+		private void ComputeHeight_OnClick(object sender, RoutedEventArgs e)
+		{
+			FinalHeight.Text = "Final H:" + GetElemHeight(b);
+		}
+
+		public static double GetElemHeight(FrameworkElement elem, double? actualWidth = null)
+		{
+			if (elem == null)
+				return 0;
+
+			// take note of the existing height, if any, since we have to re-establish it later:
+			double currentH = elem.Height;
+			if (!double.IsNaN(currentH))
+				elem.Height = double.NaN;
+			double totalW = (actualWidth ?? elem.Width) + elem.Margin.Left + elem.Margin.Right;
+
+			// Measure() only works as expected in this context if the Height is NaN:
+			elem.Measure(new Size(totalW, Double.PositiveInfinity));
+			Size size = elem.DesiredSize;
+			elem.Height = currentH; //re-establish the correct height
+			return size.Height - elem.Margin.Top - elem.Margin.Bottom;
+		}
+	}
+}
