8d4c449af570ae418ef6fefe7b4a44829779e2b0
Cristiano Ghersi <cghersi@liquidtext.net>
2020-09-11 15:56:10 -0700

    PoC for PenButtonListener

---
+++ PenButtonListener/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using Windows.Foundation.Metadata;
+using Windows.UI.Xaml.Controls;
+using Windows.Devices.Input;
+
+namespace PenButtonListener
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainPage : Page
+	{
+		private int m_buttonPressedTimes = 0;
+
+		public MainPage()
+		{
+			InitializeComponent();
+
+			if (!ApiInformation.IsTypePresent("Windows.Devices.Input.PenButtonListener"))
+				return;
+			try
+			{
+				Windows.Devices.Input.PenButtonListener.GetDefault().TailButtonClicked += OnTailButtonClicked;
+			}
+			catch (Exception e)
+			{
+				Debug.WriteLine("Cannot support PenButtonListener due to {0}", e.Message);
+			}
+		}
+
+		private void OnTailButtonClicked(Windows.Devices.Input.PenButtonListener sender, PenTailButtonClickedEventArgs args)
+		{
+			Results.Text = string.Format("Tail Button Pressed {0} times", ++m_buttonPressedTimes);
+		}
+	}
+}
